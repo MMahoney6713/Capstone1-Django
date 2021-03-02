44595a825eb23653ea8b7ae59517b21e8a652da4
mmahoney6713 <mrm257@cornell.edu>
2021-03-02 12:58:33 -0500

    connected models and created fixture file to seed the database

--- Capstone1/calendars/models.py
+++ Capstone1/calendars/models.py
@@ -22,3 +22,34 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+
+class Users(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=15, unique=True)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    # image = models.ImageField()
+
+
+class Missions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    # color
+
+class Goals(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    # color
+
+class Milestones(models.Model):
+    id = models.IntegerField(primary_key=True)
+    goal = models.ForeignKey(Goals, on_delete=models.CASCADE)
+    date = models.DateField()
+
