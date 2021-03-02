7ba37328706408a402f324d4893c8fc5f9c8b248
mmahoney6713 <mrm257@cornell.edu>
2021-03-01 21:22:27 -0500

    added reference imports for later use

--- Capstone1/calendars/views.py
+++ Capstone1/calendars/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 
 def homepage(request):
     return render(request, 'calendars/homepage.html')
+
+
+def exampleRedirect(request, question_id):
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
